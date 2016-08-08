bf14a6f7e060ca95ce54091d506bd9e02dee3e31
Nikolay Markin <markoni.nik@gmail.com>
2016-08-08 14:32:27 +0300

    Реализован метод, копирующий состояние объекта-исходника, в объект приемника используя Reflection API

--- HW06/src/ru/sbt/Main.java
+++ HW06/src/ru/sbt/Main.java
@@ -4,34 +4,7 @@ import ru.sbt.domain.TestClassWithConsts;
 import ru.sbt.reflections.Utils;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Utils.checkStringConstNameConvention(new TestClassWithConsts());
-        // write your code here
-
-
-        /*
-        * public class BeanUtils {
-    /**
-     * Scans object "from" for all getters. If object "to"
-     * contains correspondent setter, it will invoke it
-     * to set property value for "to" which equals to the property
-     * of "from".
-     * <p/>
-     * The type in setter should be compatible to the value returned
-     * by getter (if not, no invocation performed).
-     * Compatible means that parameter type in setter should
-     * be the same or be superclass of the return type of the getter.
-     * <p/>
-     * The method takes care only about public methods.
-     *
-     * @param to   Object which properties will be set.
-     * @param from Object which properties will be used to get values.
-     */
-//        public static void assign(Object to, Object from) {... }
-//    }
-
-
     }
 }
